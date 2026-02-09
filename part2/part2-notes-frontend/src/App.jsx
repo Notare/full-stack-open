e50dcfba5aeea86cebed3c48089b19fbd901be2a
Renato Sousa <renatonotare@gmail.com>
2026-02-09 00:16:34 -0300

    Add 2.10: The Phonebook Step 5

---
+++ part2/part2-notes-frontend/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import Note from "./components/Note";
+
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState("a new note...");
+  const [showAll, setShowAll] = useState(true);
+
+  const addNote = (ev) => {
+    ev.preventDefault();
+    const noteObj = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: String(notes.length + 1),
+    };
+
+    setNotes(notes.concat(noteObj));
+    setNewNote("");
+  };
+
+  const handleNoteChange = (ev) => {
+    console.log(ev.target.value);
+    setNewNote(ev.target.value);
+  };
+
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? "important" : "all"}{" "}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} type="text" />
+        <button type="submit">save</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
